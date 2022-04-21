637b43c4d5866f2f24bc2a0a5bec0a0cd4b7615a
Barry-owino <barryowino5@gmail.com>
2022-04-22 02:00:33 +0300

    added the opacity to the images

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const current = document.querySelector('#current');
 const photos = document.querySelectorAll('.photos img');
-const opacity = 0.3;
+const opacity = 0.4;
 
 photos.forEach(img => img.addEventListener('click', e => (current.src = e.target.src)));
 
 function imgClick(e) {
+  //Reset the opacity
+  photos.forEach(img => (img.style.opacity = 1));
+
   //change current image to src of clicked image
   current.src = e.target.src;
 
