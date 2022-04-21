83afe2e5836b36a650228c85b63e8ff092d185b1
Barry-owino <barryowino5@gmail.com>
2022-04-22 02:11:24 +0300

    created fadeIn animation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const current = document.querySelector('#current');
 const photos = document.querySelectorAll('.photos img');
+
 const opacity = 0.4;
 
 photos.forEach(img => img.addEventListener('click', e => (current.src = e.target.src)));
